dc73866b04bd11a7767a6885e7b624658a8533e7
jbi <jan.bissinger@outlook.de>
2015-11-15 16:30:16 +0100

    #3 - Initial commit of Artikel-Klassen-Branch describing the templates of the domain and rest packages and classes

---
+++ src/main/java/de/shop/artikelverwaltung/domain/Artikel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.shop.artikelverwaltung.domain;
+
+/**
+ * This is the domain class of an article. An order needs to implement at least one article.
+ * @author Jan
+ */
+public class Artikel {
+    
+}
